1033df2838b031746ceb9d8e8a061b2ba10aa12d
Luiz Gustavo <94083947+Luiz18g@users.noreply.github.com>
2022-12-02 21:26:57 -0300

    atualização

---
+++ Desafio03.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Desafio 03</h1>
+        <?php
+        for ($i = 0; $i <= 80; $i++){
+            if($i % 8 == 0){
+                echo $i. "<br>";
+            }
+        }
+        ?>
+</body>
+</html>
